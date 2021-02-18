d0c77726e1c074cb2d3c3013cbf8ba46b6e350a5
PrishaMadane <70759166+PrishaMadane@users.noreply.github.com>
2021-02-18 20:15:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var dog,happyDog, database,foodS, foodStock;
+
+function preload()
+{
+  Dog = loadImage('images/dogImg.png');
+  Dog2 = loadImage('images/dogImg1.png');
+}
+
+function setup() {
+
+  createCanvas(500, 500);
+  database= firebase.database();
+  dog = createSprite(250,250,10,10);
+  dog.addImage("Dog");
+  dog.scale = 0.19;
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+}
+
+
+function draw() {  
+  
+  background(46,139,87);
+ 
+ if(keyWentDown(UP_ARROW)){
+   writeStock(foodS);
+   dog.addImage(Dog1);
+ }
+
+ 
+ 
+ drawSprites();
+  textSize(20);
+  fill("white");
+  text("Note: Press UP_ARROW key To feed drago Milk",100,50);
+
+}
+
+function readstock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
+
+
+
+
+
